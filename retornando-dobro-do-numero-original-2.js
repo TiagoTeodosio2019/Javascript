9949d3187a04677bf75fed71f1f552e337628a2e
TiagoTeodosio2019 <45829208+TiagoTeodosio2019@users.noreply.github.com>
2022-09-15 18:20:24 -0300

    Add files via upload

---
+++ retornando-dobro-do-numero-original-2.js
@@ -0,0 +1,5 @@
+const numbers=[1,2,3]
+const doubleNumbers=numbers.map(item=>{
+    return item*3
+})
+console.log(doubleNumbers);
---
+++ retornando-dobro-do-numero-original.js
@@ -0,0 +1,5 @@
+//retornando o dobro do número original
+const numbers = [1,2,3]
+numbers.map(item => {
+    console.log(item*2)
+})
